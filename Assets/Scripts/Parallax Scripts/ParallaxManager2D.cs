2c4ee972ee620d01d559f61c6c49aac863b785e8
MBarselos <122841551+MBarselos@users.noreply.github.com>
2025-12-01 13:07:37 -0300

    att mecanica principal e inicio parallax 2d

---
+++ Assets/Scripts/Parallax Scripts/ParallaxManager2D.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ParallaxManager2D : MonoBehaviour
+{
+
+    [SerializeField] private Transform cam; // main camera
+    [SerializeField] private Vector3 camStartPos;
+    [SerializeField] private float distance;
+
+    [SerializeField] private GameObject[] background;
+    [SerializeField] private Material[] mat;
+    [SerializeField] private float[] backSpeed;
+
+    float farthestBack;
+
+    [Range(0.01f, 0.5f)]
+    public float parallaxSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cam = Camera.main.transform;
+        camStartPos = cam.position;
+
+        int backCount = transform.childCount;
+        mat = new Material[backCount]; 
+        backSpeed = new float[backCount];
+        background = new GameObject[backCount];
+
+        for (int i  = 0; i < backCount; i++)
+        {
+            background[i] = transform.GetChild(i).gameObject;
+            mat[i] = background[i].GetComponent<Renderer>().material;
+        }
+        BackSpeedCalculate(backCount);
+    }
+
+    void BackSpeedCalculate(int backCount)
+    {
+
+        for (int i = 0; i < backCount; i++)
+        {
+            if ((background[i].transform.position.z - cam.position.z) > farthestBack)
+                farthestBack = background[i].transform.position.z - cam.position.z;
+        }
+
+        for (int i = 0; i < backCount; i++)
+            backSpeed[i] = 1 - (background[i].transform.position.z - cam.position.z / farthestBack);
+    }
+
+    private void LateUpdate()
+    {
+        distance = cam.position.x - camStartPos.x;
+        for (int i = 0; i < background.Length; i++)
+        { 
+            float speed = backSpeed[i] * parallaxSpeed;
+            mat[i].SetTextureOffset("_MainTex", new Vector2(speed, 0)*speed);
+        }
+    }
+}
